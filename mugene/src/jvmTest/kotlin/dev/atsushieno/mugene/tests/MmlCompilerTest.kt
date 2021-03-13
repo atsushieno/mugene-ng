f9046af5ba2046f1485f49b7a7af7b219050771c
atsushieno <atsushieno@gmail.com>
2021-03-14 04:28:22 +0900

    add next compilation test that we're facing error now.

--- mugene/src/jvmTest/kotlin/dev/atsushieno/mugene/tests/MmlCompilerTest.kt
+++ mugene/src/jvmTest/kotlin/dev/atsushieno/mugene/tests/MmlCompilerTest.kt
@@ -6,6 +6,7 @@ import kotlin.test.Test
 class MmlCompilerTest {
     @Test
     fun simpleCompilation() {
-        MmlTestUtility.testCompile ("SimpleCompilation", "1   o5cde")
+        MmlTestUtility.testCompile("SimpleCompilation", "1   o5cde")
+        MmlTestUtility.testCompile("Macro definition", "#macro\tCH_INIT ch:number { CH\$ch E127 B0 P64 M0 H0 RSD0 CSD0 DSD0 v80 l8 q16 }")
     }
 }

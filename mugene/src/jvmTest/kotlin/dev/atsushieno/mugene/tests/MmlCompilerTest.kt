f9e5be8e785f89590d8943d30b554901137a2505
atsushieno <atsushieno@gmail.com>
2021-03-15 02:10:08 +0900

    Add more checks in `simpleTrackAndNotes()` to examine the actual SMF output.
    And it does not pass yet. We need further fixes.

--- mugene/src/jvmTest/kotlin/dev/atsushieno/mugene/tests/MmlCompilerTest.kt
+++ mugene/src/jvmTest/kotlin/dev/atsushieno/mugene/tests/MmlCompilerTest.kt
@@ -2,10 +2,22 @@
 package dev.atsushieno.mugene.tests
 
 import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class MmlCompilerTest {
     @Test
-    fun simpleTrackAndNotes() { MmlTestUtility.testCompile("SimpleCompilation", "1   o5cde")
+    fun simpleTrackAndNotes() {
+        val bytes = MmlTestUtility.testCompile("SimpleCompilation", "1   o5cde")
+        val expected = intArrayOf(
+            'M'.toInt(), 'T'.toInt(), 'h'.toInt(), 'd'.toInt(), 0, 0, 0, 6, 0, 1, 0, 1, 0, 0x30,
+            'M'.toInt(), 'T'.toInt(), 'r'.toInt(), 'k'.toInt(), 0, 0, 0, 0x1C,
+            0, 0x90, 0x3C, 100,
+            0x30, 0x80, 0x3C, 0,
+            0, 0x90, 0x3E, 100,
+            0x30, 0x80, 0x3E, 0,
+            0, 0x90, 0x40, 100,
+            0x30, 0x80, 0x40, 0).map { i -> i.toByte() }.toByteArray()
+        assertEquals(expected, bytes, "MIDI bytes")
     }
     @Test
     fun simpleMacroDefinition() {

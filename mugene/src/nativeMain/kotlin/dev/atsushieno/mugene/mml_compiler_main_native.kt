765200163331dd34920264949af71a9c97a48137
atsushieno <atsushieno@gmail.com>
2021-05-11 19:28:06 +0900

    implemented stream resolver in nativeMain. Now all tests should pass.

--- mugene/src/nativeMain/kotlin/dev/atsushieno/mugene/mml_compiler_main_native.kt
+++ mugene/src/nativeMain/kotlin/dev/atsushieno/mugene/mml_compiler_main_native.kt
@@ -1,13 +1,25 @@
 package dev.atsushieno.mugene
 
+import kotlinx.cinterop.addressOf
+import kotlinx.cinterop.convert
+import kotlinx.cinterop.usePinned
+import platform.posix.fclose
+import platform.posix.fopen
+import platform.posix.fwrite
+
 class MmlCompilerNative : MmlCompilerConsole() {
-    override var resolver : StreamResolver = MergeStreamResolver(LocalFileStreamResolver())
+    override var resolver : StreamResolver = MergeStreamResolver(NativeDevResourceStreamResolver(), LocalFileStreamResolver())
 
     override fun decodeStringUsingEncoding(s: String, charset: String): ByteArray =
         TODO("Not Implemented")
 
-    override fun writeToFile(filename: String, bytes: ByteArray) =
-        TODO("Not Implemented")
+    override fun writeToFile(filename: String, bytes: ByteArray) {
+        val fp = fopen(filename, "w+") ?: throw IllegalArgumentException("File '$filename' is not writable")
+        bytes.usePinned { bytesPinned ->
+            fwrite(bytesPinned.addressOf(0), bytes.size.convert(), 1.convert(), fp)
+        }
+        fclose(fp)
+    }
 }
 
 internal actual fun createDefaultCompiler() : MmlCompiler = MmlCompilerNative()
--- mugene/src/nativeMain/kotlin/dev/atsushieno/mugene/mml_stream_resolver_native.kt
+++ mugene/src/nativeMain/kotlin/dev/atsushieno/mugene/mml_stream_resolver_native.kt
@@ -1,12 +1,66 @@
 package dev.atsushieno.mugene
 
+import kotlinx.cinterop.ByteVar
+import kotlinx.cinterop.allocArray
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.toKString
+import platform.posix.fclose
+import platform.posix.fgets
+import platform.posix.fopen
+import platform.posix.realpath
+import platform.posix.stat
+
 //region input sources to tokenizer sources
 
-class LocalFileStreamResolver : StreamResolver() {
+open class LocalFileStreamResolver : StreamResolver() {
 
-    override fun resolveFilePath(file: String): String? =
-        TODO("Not Implemented")
+    private fun exists(file: String) : Boolean = stat(file, null) == 0
+    private fun getRealpath(file: String) : String {
+        memScoped {
+            val buffer = allocArray<ByteVar>(4096)
+            realpath(file, buffer)
+            return buffer.toKString()
+        }
+    }
+    override fun resolveFilePath(file: String): String? {
+        if (!includes.any()) {
+            if (!exists(file))
+                return file
+            return getRealpath(file)
+        }
+        if (exists(file) && getRealpath(file) == file)
+            return file
+        return getRealpath(includes.last() + '/' + file)
+    }
 
-    override fun onGetEntity(file: String): String? =
-        TODO("Not Implemented")
+    override fun onGetEntity(file: String): String? {
+        val filePath = resolveFilePath(file)
+
+        val returnBuffer = StringBuilder()
+        val fp = fopen(filePath, "r") ?: throw IllegalArgumentException("Cannot open '$filePath'")
+
+        try {
+            memScoped {
+                val bufferSize = 64 * 1024
+                val buffer = allocArray<ByteVar>(bufferSize)
+                var line = fgets(buffer, bufferSize, fp)?.toKString()
+                while (line != null) {
+                    returnBuffer.append(line)
+                    line = fgets(buffer, bufferSize, fp)?.toKString()
+                }
+            }
+        } finally {
+            fclose(fp)
+        }
+
+        return returnBuffer.toString()
+    }
+}
+
+// I assume this only works for dev. environment for unit testing. We need more paths to resolve...
+class NativeDevResourceStreamResolver : LocalFileStreamResolver() {
+
+    override fun resolveFilePath(file: String): String? {
+        return super.resolveFilePath("build/processedResources/native/main/$file")
+    }
 }
